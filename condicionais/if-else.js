8b5c5a0e25648629cd61777e4f2ece4264e6438a
camillamelo-hash <camilla.melo@escola.pr.gov.br>
2026-03-25 11:41:37 +0000

    atualização de arquivos js

---
+++ condicionais/if-else.js
@@ -0,0 +1,16 @@
+// if, else if, else, termário
+// >, <, >=, <=, !=
+// ||, &&
+
+let idade = 116
+
+if(idade > 110){
+    console.log("Situação impar")
+    console.log("Traga um acompanhante")
+} else if (idade >= 17 && idade <= 110 ) {
+    console.log("Entrada liberada")
+    console.log("Festa sugerida:Balada!!!")
+} else {
+    console.log("Proibido, vaza menor!")
+    console.log("Festa recomenda: Monica Toy")
+}

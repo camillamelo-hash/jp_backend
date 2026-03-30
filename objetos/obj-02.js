6bf9b296b1556892dfef1aa90efe24d9e0ef18bb
camillamelo-hash <camilla.melo@escola.pr.gov.br>
2026-03-30 12:35:41 +0000

    finalizaçao objetos

--- objetos/obj-02.js
+++ objetos/obj-02.js
@@ -3,3 +3,12 @@
 // Deve possuir no minimo 6 propriedades
 // Imprima de um jeito amigavel:
 // Ex.: console.log ("Cor do veiculo:" + carro.cor)
+
+const gol = {
+   nome: gol,
+   cor: preto ,
+   valor:15.000
+}
+console.log("nome do carro + gol.nome")
+console.log("cor do carro:" + gol.cor)
+console.log("valor do carros:" + gol.valor)
---
+++ variaveis/var-pessoa.js
@@ -0,0 +1,18 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Petter"        // string 
+let sobrenome = "Parker"  //string
+const cpf = "88888888-98" //string
+const rg = "132131 ssp"   //string
+let idade = 17            //string
+let namorando = true      //string
+let nota  = 5  + 3        //string
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(namorando)
+console.log(nota)

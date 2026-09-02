2fd57dcb753005e3cb57890c740a050df51ffcdc
camillamelo-hash <camilla.melo@escola.pr.gov.br>
2026-09-02 12:31:55 +0000

    feat: initialize express application with basic routes and package configuration

--- 3°TRIMESTRE/express01/servidor.js
+++ 3°TRIMESTRE/express01/servidor.js
@@ -1,13 +1,13 @@
 // importação moderna com ESM (import)
 import express from 'express'
 
-const app = express()
+const app = express() // Instanciar express
 
 app.get('/', (req, res) => {
-    res.send('Home Ok!')
+    res.send(`<h3>Página inicial</h3>\n<p>Loja especializada em sapatos</p>`)
 })
 app.get('/sapatos', (req, res) => {
-    res.send('meu sapato de sair!')
+    res.send('Meu sapato de sair...!')
 })
 
 app.listen(3000, ()=> {console.log('Servidor está vivo!')})
